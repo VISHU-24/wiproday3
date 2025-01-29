32b0519f810b7bd7c6efa2153376f6f91c4de370
VISHAL SAMPETA <102171620+VISHU-24@users.noreply.github.com>
2025-01-29 12:14:15 +0530

    fisrt

---
+++ AccessSpecifierdemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using day3;
+
+namespace AccessSpecifierdemo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Customer cst = new Customer();
+            cst.custId = 11;
+            cst.custName = "Liya";
+            cst.Display();
+        }
+    }
+}
